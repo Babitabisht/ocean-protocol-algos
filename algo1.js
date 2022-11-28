df8c196a7294cda3ddb218f85f3135257fdefbb7
Babita Bisht <babitabisht2018@gmail.com>
2022-11-28 13:40:54 +0530

    Update algo1.js

--- algo1.js
+++ algo1.js
@@ -8,14 +8,13 @@ const outputFolder = '/data/outputs/'
 async function processfolder(folder) {
 
  
-      const files = fs.readdirSync(folder);
-       console.log("this is the value of folder ", files);
-       console.log("this is the custom file ", "."+folder+files[0]);
-      const data = await fs.promises.readFile("."+folder+files[0], 'utf8')
+//       const files = fs.readdirSync(folder);
+//        console.log("this is the value of folder ", files);
+//        console.log("this is the custom file ", "."+folder+files[0]);
+      const data = await fs.promises.readFile('./data/inputs/algoCustomData.json', 'utf8')
       console.log("data---->",data);
  
-      const data2 = await fs.promises.readFile("."+folder+files[1], 'utf8')
-      console.log("data2---->",data2);
+
     
       
 }
