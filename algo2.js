7e2643e633b5667a9b5722018d5190bc351587fb
Global <global@del1-lmc-n72979.local>
2022-11-29 10:13:46 +0530

    carbon data added

---
+++ algo2.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+
+const inputFolder = '/data/inputs'
+const outputFolder = '/data/outputs'
+
+
+async function compute( file) {
+ 
+  let data = await fs.promises.readFile(file, 'utf8')
+  data = JSON.parse(data);
+  let result; 
+  if(Array.isArray(data)&& data.length>0){
+    result = data.map( item => {
+        return item.email;
+    } );
+  }
+  console.log('Finished. We have this data  ' + result )
+  let finalOutput = result!=undefined? " "+`######################################  ${result} ##################################################`: " ";
+  fs.appendFileSync(outputFolder + '/output.log', file + ',' + finalOutput + '\r\n' )
+}
+
+async function processfolder(folder) {
+  const files = fs.readdirSync(folder)
+  
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i]
+    const fullpath = folder + '/' + file;
+
+   
+    if (fs.statSync(fullpath).isDirectory()) {
+      await processfolder(fullpath)
+    } else {
+      await compute( fullpath)
+    }
+  }
+}
+
+
+processfolder(inputFolder)
