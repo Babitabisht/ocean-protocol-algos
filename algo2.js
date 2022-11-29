9e25a123b4245970934874dcce3fb60a69124222
Babita Bisht <babitabisht2018@gmail.com>
2022-11-29 10:36:48 +0530

    Update algo2.js

--- algo2.js
+++ algo2.js
@@ -6,17 +6,19 @@ const outputFolder = '/data/outputs'
 
 async function compute( file) {
  
-  let data = await fs.promises.readFile(file, 'utf8')
+ console.log("file: " + file);
+  let data = await fs.promises.readFile(file, 'utf8');
   data = JSON.parse(data);
   let result; 
+
   if(Array.isArray(data)&& data.length>0){
-    result = data.map( item => {
-        return item.email;
-    } );
+    result = 'Electricity consumtion '+ data.reduce((total,  currentItem) => total + currentItem.electricity_consumption, 0 );
+
   }
   console.log('Finished. We have this data  ' + result )
   let finalOutput = result!=undefined? " "+`######################################  ${result} ##################################################`: " ";
-  fs.appendFileSync(outputFolder + '/output.log', file + ',' + finalOutput + '\r\n' )
+  fs.appendFileSync(outputFolder + '/output.log', file + ',' + finalOutput + '\r\n' );
+
 }
 
 async function processfolder(folder) {
