5fd3ec7f9a6662e335aaad4ca84dcaf9f81159d3
Babita Bisht <babitabisht2018@gmail.com>
2022-12-05 11:15:42 +0530

    Update carbon_computation_algo.js

--- carbon_computation_algo.js
+++ carbon_computation_algo.js
@@ -9,12 +9,15 @@ async function compute( file) {
  console.log("file: " + file);
   let data = await fs.promises.readFile(file, 'utf8');
   data = JSON.parse(data);
+ 
+  // removing duplicate entries
+  data=Object.values(data.reduce((acc,cur)=>Object.assign(acc,{[cur.product_id]:cur}),{}));
   let result; 
 
   if(Array.isArray(data)&& data.length>0){
-    result  = 'Electricity consumtion : '+ data.reduce((total,  currentItem) => total + currentItem.energy_consumed, 0 ) + " kWh";
+    result  = 'Electricity consumtion : '+ roundOf(data.reduce((total,  currentItem) => total + currentItem.energy_consumed, 0 )) + " kWh";
     result += ",    ";
-    result += 'Carbon Emission  : '+ data.reduce((total,  currentItem) => total + currentItem.carbon_emissions_tons, 0 ) +  " metric tons";
+    result += 'Carbon Emission  : '+  roundOf(data.reduce((total,  currentItem) => total + currentItem.carbon_emissions_tons, 0 ))  +  " metric tons";
   }
  
 
@@ -23,6 +26,10 @@ async function compute( file) {
   let finalOutput = result!=undefined? " Finished  "+`######################################  ${result} ##################################################`: " ";
   fs.appendFileSync(outputFolder + '/output.log', file + ',' + finalOutput + '\r\n' );
 
+}
+
+async function roundOf(value){
+ return  Math.round((value + Number.EPSILON) * 100) / 100;
 }
 
 async function processfolder(folder) {
